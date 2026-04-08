6a25ec77a8b016b6b38c42ba951be53273c418e6
Akau Bior <akaubior133@gmail.com>
2026-04-08 15:16:28 +0300

    fix port binding for render

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def predict():
     prediction = model.predict([list(data.values())])
     return jsonify({"prediction": int(prediction[0])})
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
